9d91a91c776aea0eca88100c3927ec9c1021dc20
arpitonoian <arpitonoian@gmail.com>
2020-12-22 19:25:02 +0400

    commit

---
+++ src/SingletonDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CounterService counterService = CounterService.GetInstance;
+            counterService.Add();
+            CounterService counterService1 = CounterService.GetInstance;
+            counterService1.Add();
+            CounterService counterService2 = CounterService.GetInstance;
+            counterService2.Add();
+            counterService.Print();
+        }
+    }
+}
